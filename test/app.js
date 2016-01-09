32fbd7458173be8178ad91da3f39e537d795c7c0
Olivier Kautz <olivier@k3z.fr>
2016-01-09 15:09:53 +0100

    Updated test file

--- test/app.js
+++ test/app.js
@@ -1,4 +1,4 @@
-var md = '## de Finibus Bonorum et Malorum\n\n![Sed ut perspiciatis](pg14.jpg){: class="img-half pull-right"}Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi **[architecto beatae vitae](https://en.wikipedia.org/wiki/Architect){: class="text-success" }** dicta sunt explicabo. _Nemo enim ipsam voluptatem quia voluptas_ sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.{: class="lead"}\n\n### Neque porro\n\nNeque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. {: nothing to do here }Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, *vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?*\n\n';
+var md = '## de Finibus Bonorum et Malorum\n\n![Sed ut perspiciatis](pg14.jpg){: class="img-small pull-right"}Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi **[architecto beatae vitae](https://en.wikipedia.org/wiki/Architect){: class="text-success" }** dicta sunt explicabo. _Nemo enim ipsam voluptatem quia voluptas_ sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. [architecto beatae vitae](https://en.wikipedia.org/wiki/Architect.){: class="text-success" }\n{: class="lead"}\n\n### Neque porro {: class="text-danger"}\n\n![Sed ut perspiciatis](pg14.jpg){: class="img-responsive"}\n{: class="pull-block-left"}\n\nNeque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. {: nothing to do here }Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, *vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?*\n\n';
 
 
 $(function() {
@@ -27,6 +27,8 @@ $(function() {
   md = parser.toMarkdown(html);
   $('#md-convert').html(md);
 
+  html = parser.toHTML(md);
+  $('#html-convert').html(html);
 });
 
 
